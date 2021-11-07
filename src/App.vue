15ab51a6df4c2855ac79ea828d465bebaa1f225e
cnhjp <1304272317@qq.com>
2021-11-07 23:57:01 +0800

    ✨ feat: add dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <script lang="ts" setup>
 import TodoAdd from '@/components/todo-add.vue'
 import todoList from './components/todo-list.vue';
+import { ref, Ref, watch } from 'vue-demi';
+import { useLocalStorage } from '@vueuse/core';
+
+const container: Ref<HTMLElement | null> = ref(document.documentElement)
+
+const isDarkMode: Ref<boolean> = useLocalStorage('dark-mode', false) as Ref<boolean>
+const toggleDarkMode = () => {
+  isDarkMode.value = !isDarkMode.value
+}
+watch(() => isDarkMode.value, (isDark: boolean) => {
+  if (isDark) {
+    container.value?.classList.add('dark')
+  } else {
+    container.value?.classList.remove('dark')
+  }
+}, {
+  immediate: true,
+})
+
 </script>
 
 
 <template>
-  <div class="container w-full flex flex-col items-center pt-8 font-mono">
+  <div
+    class="container h-screen overflow-y-scroll w-full flex flex-col items-center pt-8 font-mono dark:bg-gray-400"
+  >
+    <button @click="toggleDarkMode">toggle dark/light mode</button>
     <div class="title text-3xl font-bold">Easy Todo List</div>
     <todo-add class="mt-8 w-10/12 md:w-1/2 flex justify-center"></todo-add>
     <todo-list class="mt-8 w-11/12 md:w-2/3 flex justify-center"></todo-list>
